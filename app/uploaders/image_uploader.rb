25c2eaf369ca339c916361953459902929ed55a1
shuji38 <shuji_3838@icloud.com>
2020-03-03 12:51:32 +0900

    S2

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -33,6 +33,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   process resize_to_fit: [800, 800]
   # end
 
+  storage :fog
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_whitelist
